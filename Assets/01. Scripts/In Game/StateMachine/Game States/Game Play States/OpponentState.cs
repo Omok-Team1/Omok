121bd248f410476646cef13c428deb3bdfbd1181
MunHwaDong <coming2430@gmail.com>
2025-03-31 01:34:50 +0900

    디버깅용 주석 삭제

--- Assets/01. Scripts/In Game/StateMachine/Game States/Game Play States/OpponentState.cs
+++ Assets/01. Scripts/In Game/StateMachine/Game States/Game Play States/OpponentState.cs
@@ -38,41 +38,11 @@ public class OpponentState : IState
         
         coordi = await GameManager.Instance.OpponentController.BeginOpponentTask(cts);
 
-        //await UniTask.WaitUntil(() => task.Status is UniTaskStatus.Succeeded || task.Status is UniTaskStatus.Canceled);
         
-        //coordi = await GameManager.Instance.OpponentController.BeginOpponentTask(cts);
-
-        // await UniTask.WaitUntil(() =>
-        // {
-        //     //양의 무한대인 경우 : 아직 AI 연산 중, 연산이 끝나거나 Time-out인 경우 bestMove 좌표거나 음의 무한대를 던져준다.
-        //     // if (coordi.Item1 != (int)1e9 || coordi.Item2 != (int)1e9)
-        //     // {
-        //     //     return true;
-        //     // }
-        //     // return false;
-        //     return task.Status is UniTaskStatus.Succeeded || task.Status is UniTaskStatus.Canceled;
-        // });
-        
-        // //값이 전달 되거나, token에 의해 취소 될 때까지 대기
-        // GameManager.Instance.BoardManager.RecordDrop(coordi);
-        //
-        // if (cts.IsCancellationRequested)
-        // {
-        //     Debug.Log("Opponent timed out.");
-        // }
-        // else
-        // {
-        //     Debug.Log("Opponent OnDrop!!!!!!!.");
-        //     EventManager.Instance.PopLastEventMessageEvent();
-        //     GameManager.Instance.TimerController.EndPlayerTurn();
-        // }
-        //
-        // StateMachine.ChangeState<OnDropState>();
     }
 
     public void UpdateState()
     {
-        //Do nothing at Unity Update Loop...
         //값이 양의 무한대가 아니라면, 시간 초과이거나, 연산을 완료해서 정상적인 좌표를 가지고 있다.
         //Timer의 코루틴이 null이면 Timer의 세팅이 끝났다.
         if (coordi.Item1 != INF || coordi.Item2 != INF)
@@ -99,27 +69,6 @@ public class OpponentState : IState
     {
 
     }
-
-    // private IEnumerator WaitTimerSettingAndRun()
-    // {
-    //     GameManager.Instance.BoardManager.RecordDrop(coordi);
-    //
-    //     //Time-out case
-    //     if (coordi.Item1 == -INF || coordi.Item2 == -INF)
-    //     {
-    //         Debug.Log("Opponent timed out.");
-    //     }
-    //     else
-    //     {
-    //         Debug.Log("Opponent OnDrop!!!!!!!.");
-    //         EventManager.Instance.PopLastEventMessageEvent();
-    //         GameManager.Instance.TimerController.EndPlayerTurn();
-    //     }
-    //
-    //     StateMachine.ChangeState<OnDropState>();
-    //
-    //     yield return null;
-    // }
 
     private readonly int INF = (int)1e9;
     
--- Assets/01. Scripts/Utill/Scene/Runtime/TaskQueue.cs
+++ Assets/01. Scripts/Utill/Scene/Runtime/TaskQueue.cs
@@ -25,55 +25,6 @@ public class TaskQueue
         }
     }
 
-    public UniTask<T> BeginTask<T>(Func<CancellationToken, UniTask<T>> task, CancellationTokenSource token = default(CancellationTokenSource))
-    {
-        //비동기 작업의 결과를 수동으로 제어하고 알리는데 필요한 클래스
-        //비동기 작업의 결과를 UniTask로 변환하는데 쓸 수 있다.
-        var tcs = new UniTaskCompletionSource<T>();
-        
-        if (_isProcessing is true)
-        {
-            _taskQueue.Enqueue(async () =>
-            {
-                try
-                {
-                    token?.Token.ThrowIfCancellationRequested();
-                    tcs.TrySetResult(await task(token.Token));
-                }
-                catch (OperationCanceledException)
-                {
-                    Debug.LogError("작업이 취소 되었습니다.");
-                    tcs.TrySetCanceled();
-                }
-            });
-        }
-        else
-        {
-            _isProcessing = true;
-            
-            UniTask.Void(async () =>
-            {
-                try
-                { 
-                    token?.Token.ThrowIfCancellationRequested();
-                    
-                    tcs.TrySetResult(await task(token.Token));
-                }
-                catch (OperationCanceledException)
-                {
-                    tcs.TrySetCanceled();
-                }
-                finally
-                {
-                    token.Dispose();
-                    token = null;
-                }
-            });
-        }
-        
-        return tcs.Task;
-    }
-
     public void DoneTask()
     {
         _isProcessing = false;
